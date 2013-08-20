cc16d922f11686f335fb4ba6eafe7b6b684dd7e2
WillChilcutt <WillChilcutt@gmail.com>
2013-08-20 09:58:04 -0400

    Pictures now rotate correctly!

---
+++ AV Camera/UIImage+FixOrientation.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+FixOrientation.h
+//  AV Camera
+//
+//  Created by Will Chilcutt on 8/20/13.
+//  Copyright (c) 2013 ERCLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (FixOrientation)
+
+@end
---
+++ AV Camera/UIImage+FixOrientation.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+FixOrientation.m
+//  AV Camera
+//
+//  Created by Will Chilcutt on 8/20/13.
+//  Copyright (c) 2013 ERCLab. All rights reserved.
+//
+
+#import "UIImage+FixOrientation.h"
+
+@implementation UIImage (FixOrientation)
+
+@end
